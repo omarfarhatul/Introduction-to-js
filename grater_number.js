314ab35c3076b7e57da339265e60fb8348bbb068
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 19:28:22 +0600

    Update condition

---
+++ grater_number.js
@@ -0,0 +1,8 @@
+const n1 = 10;
+const n2 = 6;
+if(n1 > n2){
+    console.log("Grater number is: ", n1);
+}
+else{
+    console.log("Grater number is: ", n2);
+}
---
+++ if_else.js
@@ -0,0 +1,15 @@
+var weight =40;
+if(weight < 20){
+    console.log('I will carry it by myself');
+}
+else{
+    console.log('I will rent a rickshaw');
+}
+
+let money = 210;
+if(money > 200){
+    console.log('Go to Cineplex');
+}
+else{
+    console.log('Go to Home');
+}

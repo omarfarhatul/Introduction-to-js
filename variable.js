486acb98ff7eec7871ebf4fdb17c0dbdca2c886c
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 10:28:20 +0600

    Added variable

---
+++ variable.js
@@ -0,0 +1,14 @@
+/*
+* Veriable ==> vary+ able
+* 5 thigs to declare a variable
+*/
+
+var weight = 55 ;
+var price = 20 ;
+var Birth_year = 2000 ;
+var money = 1000 ;
+money = 446 ;
+var age;
+console.log(weight)
+console.log(price);
+console.log(money)

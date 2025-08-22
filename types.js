a1c9cebbb88b36e00a8e86304ff42e5043b2262c
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 10:41:31 +0600

    Added types for js

---
+++ types.js
@@ -0,0 +1,21 @@
+// String type
+
+var name = "Hossain";
+console.log(name);
+
+var address = "Sonargaon, Narayanganj";
+console.log(address);
+
+var country = 'Bangladesh';
+console.log(typeof country);
+
+
+// boolean type
+var isRich = true;
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+
+var price = 34535;
+console.log(typeof price);

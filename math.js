dba0e65b82af03b677c78ceda41b8123ce152a02
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 10:06:53 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,16 @@
+/* Math needed for programming
+******************************
+
+1. Number: +ve, -ve, [50, -9, 0]
+2. Integer: 2, 88, [odd, even]: 2, 5, 11
+3.Float: 4, 5.2
+4. Operation: +, -, /, *
+5.More operation: %, [ ]
+6. percentage: 5%, 50 tk er 10% koto?
+7. sum: 5, 25, 6
+8. Avg: 34, 6, 77
+9. Max/Min: 3, 6, 77, 2
+10. Series: 1 to 10 even number
+11. Area: Triangle, circle
+12. 
+*/

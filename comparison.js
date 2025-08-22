410805f34f4008489abd28f6e70cb1c8058d59c1
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 19:08:04 +0600

    Added comparison js

---
+++ comparison.js
@@ -0,0 +1,18 @@
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(15 >= 10);
+console.log(5 >= 10);
+console.log(5 <= 10);
+console.log(10.1 == 10);
+console.log(10 == '10');
+console.log(10 === '10');
+
+// --------------------
+var peyaraPrice =40;
+var applePrice =250;
+
+console.log(peyaraPrice < applePrice);
+

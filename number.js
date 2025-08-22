b2d36354b3c59efdcd840a573f6b5ee037ce6cae
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 11:03:39 +0600

    Added number js

---
+++ number.js
@@ -0,0 +1,20 @@
+var orangePrice = 40;
+var applePrice = '34';
+var banana = parseInt('30');
+var jackFruit = parseFloat('22.5');
+
+console.log(typeof applePrice);
+console.log(typeof orangePrice);
+console.log(typeof banana);
+console.log(typeof jackFruit);
+console.log(orangePrice+applePrice);
+
+var first = 0.1;
+var second = 0.2;
+var total=first+second;
+console.log(total.toFixed(1));
+
+const num = 50/0;
+console.log(num);
+const num1 = -50/0;
+console.log(num1);

f66b3c067c22783c51bb886a3af20fc33549270f
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-08-22 11:16:01 +0600

    Update

---
+++ arithmatic_operation.js
@@ -0,0 +1,15 @@
+var n1 = 5; 
+var n2 =2;
+var add=n1+n2;
+var sub=n1-n2;
+var mul=n1*n2;
+var div=n1/n2;
+var rem=n1%n2;
+
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(rem);
+
+
